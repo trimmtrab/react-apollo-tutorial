641872e7f1d982dcac14a9989f498b236253ff77
Artem Zimin <atm.zmn95@gmail.com>
2019-08-02 17:55:46 +0300

    add CreateLink component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 function App() {
   return (
-    <LinkList />
+    <CreateLink />
   );
 }
 
