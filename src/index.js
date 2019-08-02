afa25b9de5076b9c83f33c8f30e03e825f54f877
Artem Zimin <atm.zmn95@gmail.com>
2019-08-02 19:25:42 +0300

    configure Apollo with the authentication token

--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ import React from 'react';
 
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
+import { setContext } from 'apollo-link-context';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import * as serviceWorker from './serviceWorker';
 import App from './components/App';
+import { AUTH_TOKEN } from './constants';
+import * as serviceWorker from './serviceWorker';
 import './styles/index.css';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
